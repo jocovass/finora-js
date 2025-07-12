c439ce750e66940aebeac391518107146e8f29e0
Jozsef <joco.udv@gmail.com>
2025-07-12 07:37:35 +0100

    Add AWS parameter store

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { name } from '@repo/validation';
 import express from 'express';
 
@@ -5,8 +6,12 @@ import { app as appp } from './app';
 
 const app = express();
 
+app.get('/health', (req, res) => {
+	res.send(`We are up and running 🎉 ${process.env.NODE_ENV!}`);
+});
+
 app.listen(8000, () => {
-	console.log('The author is ', name);
+	console.log('The author iss ', name);
 	console.log(JSON.stringify(appp));
 	console.log('API is running on port 8000');
 });
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd /home/ec2-user/finora-api
 
+# Fetch all parameters at once
+PARAMS=$(aws ssm get-parameters-by-path --path "/finora-api" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text)
+
+# Export as environment variables
+while read -r name value; do
+    var_name=$(echo "$name" | sed 's|/finora-api/||')
+    echo "$var_name=$value" >> .env
+done <<< "$PARAMS"
+
 echo "Starting API server..."
 # Start your built API server
 nohup node apps/api/dist/index.cjs > /tmp/api.log 2>&1 &
